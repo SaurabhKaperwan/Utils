0f7f986ef8e131c8eda5eee07728517922ebdda7
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2026-04-30 20:42:35 +0530

    Update url-checker.js

--- .github/scripts/url-checker.js
+++ .github/scripts/url-checker.js
@@ -144,7 +144,7 @@ async function main() {
   const providers = urlsJson();
   let hasChanges = false;
 
-  const SKIP_KEYS = new Set(['nfmirror', 'nfmirror2', 'hianime']);
+  const SKIP_KEYS = new Set(['nfmirror']);
   // Process each provider
   for (const [name, url] of Object.entries(providers)) {
 
